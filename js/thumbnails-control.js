a01cdcace7119eb249bcb73775fb5c5f0b167bdc
fluffy <klim44@inbox.ru>
2024-12-17 01:16:24 +0500

    Реализована работа fetch

--- js/thumbnails-control.js
+++ js/thumbnails-control.js
@@ -1,26 +1,26 @@
-import { createPhoto } from './photo-control.js';
 import { showFullPicture } from './fullwindow-photo.js';
 
-const PHOTOS = Array.from({length: 25}, createPhoto);
 const thumbnails = document.querySelector('.pictures');
 const thumbnailTemplate = document.querySelector('#picture').content.querySelector('a');
 const thumbnailsContainer = document.createDocumentFragment();
 
-const drawThumbnails = function() {
-  for (let i = 0; i < PHOTOS.length; i++) {
+const drawThumbnails = function(photos) {
+  for (let i = 0; i < photos.length; i++) {
     const thumb = thumbnailTemplate.cloneNode(true);
 
-    thumb.querySelector('img').src = PHOTOS[i].url;
-    thumb.querySelector('img').alt = PHOTOS[i].description;
-    thumb.querySelector('.picture__comments').textContent = PHOTOS[i].comments.length;
-    thumb.querySelector('.picture__likes').textContent = PHOTOS[i].likes;
+    thumb.querySelector('img').src = photos[i].url;
+    thumb.querySelector('img').alt = photos[i].description;
+    thumb.querySelector('.picture__comments').textContent = photos[i].comments.length;
+    thumb.querySelector('.picture__likes').textContent = photos[i].likes;
     thumb.querySelector('img').id = i;
     thumbnailsContainer.appendChild(thumb);
   }
 
   thumbnails.appendChild(thumbnailsContainer);
   thumbnails.addEventListener('click', (evt) =>{
-    showFullPicture(PHOTOS[evt.target.id]);
+    if (evt.target.id !== '') {
+      showFullPicture(photos[evt.target.id]);
+    }
   });
 };
 

2ce15f5c56018db3296f061398d7596775741e3a
fluffy <klim44@inbox.ru>
2024-12-22 19:45:30 +0500

    Добавлена сортировка

---
+++ js/sort-control.js
@@ -0,0 +1,65 @@
+import { generateRandomUniqueIntArray, errorAlert, debounce } from './util.js';
+import { drawThumbnails } from './thumbnails-control.js';
+import { getData } from './data-control.js';
+const RANDOM_SORT_AMOUNT = 10;
+const MIN_ID = 1;
+const MAX_ID = 24;
+const thumbnailsContainer = document.querySelector('.pictures');
+const defaultSortButton = document.querySelector('#filter-default');
+const randomSortButton = document.querySelector('#filter-random');
+const popularSortButton = document.querySelector('#filter-discussed');
+
+const renderSort = () => {
+  defaultSortButton.addEventListener('click', (evt) => {
+    const thumbnailsArr = thumbnailsContainer.querySelectorAll('a.picture');
+    thumbnailsArr.forEach((thumb) => thumb.remove());
+    document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+    evt.target.classList.add('img-filters__button--active');
+    getData()
+      .then((thumbs) => {
+        debounce(drawThumbnails(thumbs));
+      })
+      .catch(
+        (err) => {
+          errorAlert(err.message);
+        }
+      );
+  });
+
+  randomSortButton.addEventListener('click', (evt) => {
+    const ids = generateRandomUniqueIntArray(RANDOM_SORT_AMOUNT, MIN_ID, MAX_ID);
+    const thumbnailsArr = thumbnailsContainer.querySelectorAll('a.picture');
+    thumbnailsArr.forEach((thumb) => thumb.remove());
+    document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+    evt.target.classList.add('img-filters__button--active');
+    getData()
+      .then((thumbs) => {
+        const choosenThumbs = thumbs.filter((thumb) => ids.includes(thumb.id));
+        debounce(drawThumbnails(choosenThumbs));
+      })
+      .catch(
+        (err) => {
+          errorAlert(err.message);
+        }
+      );
+  });
+
+  popularSortButton.addEventListener('click', (evt) => {
+    const thumbnailsArr = thumbnailsContainer.querySelectorAll('a.picture');
+    thumbnailsArr.forEach((thumb) => thumb.remove());
+    document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+    evt.target.classList.add('img-filters__button--active');
+    getData()
+      .then((thumbs) => {
+        const sortedThumbs = thumbs.sort((a, b) => b.comments.length - a.comments.length);
+        debounce(drawThumbnails(sortedThumbs));
+      })
+      .catch(
+        (err) => {
+          errorAlert(err.message);
+        }
+      );
+  });
+};
+
+export {renderSort};
